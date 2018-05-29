a1c281bd508d2460f2d610c064e4dbe45e1ba8e0
Takao <>
2018-05-29 23:57:18 +0900

    add Home, Editor template

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
+  <div class="editor">
+    <h1>エディタ画面</h1>
   </div>
 </template>
 
 <script>
+  export default {
+    name: 'editor',
+    data() {
+      return {
 
+      }
+    }
+  }
 </script>
+
+<style lang="SCSS">
+
+</style>
